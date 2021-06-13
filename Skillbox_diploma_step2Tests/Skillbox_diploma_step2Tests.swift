c1e32393904781e0042a4be8a2370dcc45f24fad
Roman Sundurov <roman.sundurov.work@gmail.com>
2021-06-14 00:03:05 +0300

    Initial Commit

---
+++ Skillbox_diploma_step2Tests/Skillbox_diploma_step2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Skillbox_diploma_step2Tests.swift
+//  Skillbox_diploma_step2Tests
+//
+//  Created by Roman on 14.06.2021.
+//
+
+import XCTest
+@testable import Skillbox_diploma_step2
+
+class Skillbox_diploma_step2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

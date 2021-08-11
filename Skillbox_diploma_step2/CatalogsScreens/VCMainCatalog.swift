1fb6113156e9889e12adc1b0cfd707a8236098ee
Roman Sundurov <roman.sundurov.work@gmail.com>
2021-08-11 20:19:09 +0300

    In process: ProfileSliderScreen.

--- Skillbox_diploma_step2/CatalogsScreens/VCMainCatalog.swift
+++ Skillbox_diploma_step2/CatalogsScreens/VCMainCatalog.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class VCMainCatalog: UIViewController {
+    
+    @IBOutlet var slideProfileMenu: UIView!
+    @IBOutlet var menuButtonView: UIView!
+    @IBOutlet var menuButtonHorizonGesture: UIPanGestureRecognizer!
+    
+    @IBAction func handlerMenuButtonHorizonGesture(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else {
+          return
+        }
+        if gestureView.frame.origin.x + translation.x >= UIScreen.main.bounds.width - 255 && gestureView.frame.origin.x + translation.x <= UIScreen.main.bounds.width - gesture.view!.frame.size.width {
+            gestureView.center = CGPoint(
+              x: gestureView.center.x + translation.x,
+              y: gestureView.center.y
+            )
+            slideProfileMenu.center = CGPoint(
+                x: slideProfileMenu.center.x + translation.x,
+                y: slideProfileMenu.center.y
+              )
 
-//    @IBAction func rightEdgePanGsture(_ sender: Any) {
-//        print("Screen edge swiped!")
-//    }
+        }
+        
+        // 3
+        gesture.setTranslation(.zero, in: view)
+        
+        guard gesture.state == .ended else {
+          return
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

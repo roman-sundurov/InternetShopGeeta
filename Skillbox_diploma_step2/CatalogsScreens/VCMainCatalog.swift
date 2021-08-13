d2deffe9bd0718b320b45df1458f6598ae7d2763
Roman Sundurov <roman.sundurov.work@gmail.com>
2021-08-13 21:30:08 +0300

    In process: ProfileSliderScreen animation.

--- Skillbox_diploma_step2/CatalogsScreens/VCMainCatalog.swift
+++ Skillbox_diploma_step2/CatalogsScreens/VCMainCatalog.swift
@@ -9,9 +9,44 @@ import UIKit
 
 class VCMainCatalog: UIViewController {
     
+    
+    //MARK: - объявление аутлетов
+    
     @IBOutlet var slideProfileMenu: UIView!
     @IBOutlet var menuButtonView: UIView!
     @IBOutlet var menuButtonHorizonGesture: UIPanGestureRecognizer!
+    
+
+    //MARK: - делегаты и переменные
+    
+    var menuState: Bool = false
+    
+    //MARK: - объекты
+    
+    //MARK: - переходы
+    
+    //MARK: - клики
+    
+    //MARK: - данные
+    
+    //MARK: - viewDidLoad
+    
+    //MARK: - additional protocols
+    
+    
+    @IBAction func handlerButtonTapGesture(_ gesture: UITapGestureRecognizer) {
+//        if gesture.state == UIGestureRecognizer.State.ended {
+//            print("Tap menu ended")
+//            let pointOfTap = gesture.location(in: self.view)
+//            if containerBottomOperationScreen1.frame.contains(pointOfTap) {
+//                print("Tap inside Container")
+//            }
+//            else {
+//                print("Tap outside Container")
+//                actionsOperationsClosePopUpScreen1()
+//            }
+//        }
+    }
     
     @IBAction func handlerMenuButtonHorizonGesture(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: view)
@@ -33,13 +68,39 @@ class VCMainCatalog: UIViewController {
         // 3
         gesture.setTranslation(.zero, in: view)
         
+        if gesture.state == .ended {
+            if gestureView.frame.origin.x + translation.x >= UIScreen.main.bounds.width - 255/2 {
+                print("111")
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: UIView.AnimationOptions(), animations: {
+                    gestureView.frame.origin.x = UIScreen.main.bounds.width - gesture.view!.frame.size.width
+                    self.slideProfileMenu.frame.origin.x = UIScreen.main.bounds.width
+                })
+            }
+            else {
+                print("222")
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: UIView.AnimationOptions(), animations: {
+                    gestureView.frame.origin.x = UIScreen.main.bounds.width - gesture.view!.frame.size.width - 255
+                    self.slideProfileMenu.frame.origin.x = UIScreen.main.bounds.width - 255
+                })
+
+            }
+            return
+        }
+        
         guard gesture.state == .ended else {
-          return
+            return
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        menuButtonView.layer.cornerRadius = 8
+        menuButtonView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        menuButtonView.layer.borderWidth = 0
+        menuButtonView.layer.borderColor = UIColor.clear.cgColor
+        menuButtonView.clipsToBounds = true
+
         
         
     }
